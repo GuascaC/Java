f8295d92726b5374c9c5a38bf2f01289e55c37e6
GuascaC <109239941+GuascaC@users.noreply.github.com>
2023-03-14 10:04:51 -0600

    Update Java009.java

--- Java009.java
+++ Java009.java
@@ -2,6 +2,9 @@ public class Main {
   public static void main(String[] args) {
       System.out.println(Math.max(5, 10));  
       System.out.println(Math.min(5, 10));  
-    System.out.println(Math.sqrt(64));  
+      System.out.println(Math.sqrt(64));  
+      System.out.println(Math.sqrt(64));
+      System.out.println(Math.abs(-4.7));//Absolute
+      System.out.println(Math.random());
   }
 }
