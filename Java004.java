c091d2f597c6a4921910c20e8f56a8abc86791d0
GuascaC <109239941+GuascaC@users.noreply.github.com>
2023-03-14 09:08:20 -0600

    Create Java004.java

---
+++ Java004.java
@@ -0,0 +1,15 @@
+public class MyClass {
+    public static void main(String args[]) {
+        int number = 42;
+        String text = "String" ;
+        float num = 42.5f;
+        char cha = 'L';
+        boolean bool = true ;
+        
+      System.out.println(number);
+      System.out.println(text);
+      System.out.println(num);
+      System.out.println(cha);
+      System.out.println(bool);
+    }
+}
