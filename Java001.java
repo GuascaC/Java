242c5aa8f20888156e752777bd3ad28fe1512d60
GuascaC <109239941+GuascaC@users.noreply.github.com>
2023-03-14 08:43:34 -0600

    Update and rename Java001 to Java001.java

--- Java001.java
+++ Java001.java
@@ -1,4 +1,4 @@
-public class Hello { //Function name
+public /*"Miembro"*/ class Hello { //Function name
     public static void main(String args[]) {
       System.out.println("Hello World"); //"Print"
     }
