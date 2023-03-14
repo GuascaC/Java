904a79a536bc37e0ebc46b4a4c6a2da1527e6d46
GuascaC <109239941+GuascaC@users.noreply.github.com>
2023-03-14 08:59:27 -0600

    Create Java003.java

---
+++ Java003.java
@@ -0,0 +1,9 @@
+public class Basic { 
+    public static void main(String args[]) { 
+        int sum = 0;
+        for (int c = 1; c <= 10; c++){
+        sum += c;
+    }
+      System.out.println("Sum, " + sum); 
+    }
+}
