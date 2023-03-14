c3435620a74cecd92257cd318878f762c00c7def
GuascaC <109239941+GuascaC@users.noreply.github.com>
2023-03-14 09:21:15 -0600

    Update Java006.java

--- Java006.java
+++ Java006.java
@@ -3,8 +3,16 @@ public class Main {
   public static void main(String[] args) {
     int myInt = 9;
     double myDouble = myInt; // Conversión
+    
+    double myDouble2 = 9.78d;
+    int myInt2 = (int) myDouble2; // Conversión manual
+
+    System.out.println(myDouble2);   // Outputs 9.78
+    System.out.println(myInt2);      // Outputs 9
+
 
     System.out.println(myInt);      // Outputs 9
     System.out.println(myDouble);   // Outputs 9.0
   }
 }
+
