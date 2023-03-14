3496367a2faa119d28c90ffdbc8c21f0384c85ce
GuascaC <109239941+GuascaC@users.noreply.github.com>
2023-03-14 09:55:51 -0600

    Update Java008.java

--- Java008.java
+++ Java008.java
@@ -2,10 +2,15 @@
 public class Main {
   public static void main(String[] args) {
 String txt = "Hello World";
+String txt1 = "Hello";
+String txt2 = " World";
 System.out.println("The length of the txt string is: " + txt.length());
 System.out.println(txt.toUpperCase());   // 
 System.out.println(txt.toLowerCase());   // 
 System.out.println(txt.indexOf("World")); // Hallar, Cuenta espacios
 System.out.println(txt.indexOf("Hello"));
+System.out.println(txt + "."); //Concatenar1
+System.out.println(txt1.concat(txt2)); //Concatenar2
   }
 }
+
