03d2cb5fcaf747da578d23f369e2b48068978861
GuascaC <109239941+GuascaC@users.noreply.github.com>
2023-03-14 09:30:24 -0600

    Create Java007.java

---
+++ Java007.java
@@ -0,0 +1,18 @@
+public class Main {
+  public static void main(String[] args) {
+    double myInt = 9;
+    double myInt2 = 10;
+    double sum = myInt+myInt2;
+    double res = myInt-myInt2;
+    double mult = myInt*myInt2;
+    double div = myInt/myInt2;
+    double div2 = myInt/myInt2;
+    double mod = myInt%myInt2;
+    System.out.println(sum);  
+    System.out.println(res);  
+    System.out.println(mult);  
+    System.out.println(div);  
+    System.out.println(div2);  
+    System.out.println(mod);  
+  }
+}
