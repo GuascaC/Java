2ec0bb89df19373102507201e07ac4df466a8913
GuascaC <109239941+GuascaC@users.noreply.github.com>
2023-03-14 10:22:06 -0600

    Create Java011.java

---
+++ Java011.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+      int numa = 42;
+      int numb = 24;
+    if(numa > numb){
+     System.out.println( "Valor A es Mayor a Valor B");
+    }
+    if(numa != numb){
+         System.out.println( "Valor A es Diferente a Valor B");
+    }
+  }
+}
