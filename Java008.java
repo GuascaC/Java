15da3071d8de7cc3eac2e07e0efbdb94155190c5
GuascaC <109239941+GuascaC@users.noreply.github.com>
2023-03-14 09:42:10 -0600

    Create Java008.java

---
+++ Java008.java
@@ -0,0 +1,11 @@
+
+public class Main {
+  public static void main(String[] args) {
+String txt = "Hello World";
+System.out.println("The length of the txt string is: " + txt.length());
+System.out.println(txt.toUpperCase());   // 
+System.out.println(txt.toLowerCase());   // 
+System.out.println(txt.indexOf("World")); // Hallar, Cuenta espacios
+System.out.println(txt.indexOf("Hello"));
+  }
+}
